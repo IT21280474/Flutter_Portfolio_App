0db9e1e663e52e6532e2d1c4fc5d650de0bef517
Malith Madushan Liyanage <malithmadushan5112@gmail.com>
2024-02-07 06:31:23 +0530

    4th commit (Skills)

--- lib/main.dart
+++ lib/main.dart
@@ -48,16 +48,176 @@ class MyApp extends StatelessWidget {
                   child: Image.asset("assets/malith2.png",
                   ),
                 ),
+                const SizedBox(
+                  height: 30,
+                ),
                 //Button
-                Container(
-                  padding:EdgeInsets.all(15),
-                  color: Colors.redAccent,
-                  decoration: BoxDecoration(),
-                  child: Text(
-                    "My Details",
-                   style: TextStyle(color: Colors.white)
+               GestureDetector (
+                onTap: () {},
+                  child: Center(
+                    child: Container(
+                      padding: const EdgeInsets.all(15),
+                      
+                      decoration: BoxDecoration(
+                        color: const  Color.fromARGB(255, 8, 223, 195),
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                      child: const Text(
+                        "My Details",
+                       style: TextStyle(color: Colors.white,fontSize: 20,
+                    fontWeight: FontWeight.w400)
+                      ),
+                      ),
                   ),
-                  )
+                ),
+//second screen 
+                Center(
+                  child: Image.asset("assets/malith2.png",
+                  height:200 ,
+                  scale: 2,
+                  ),
+                ),
+                const Text("My name is Malith Madushan Liyanage, and I am a dedicatedsoftware engineering undergraduate student. I am a tech Enthuses and Fast Learner. I had the privilege of attending Kannangara Central College during my school years, where I developed a strong foundation in academics and a passion for technology.",
+                textAlign:TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                  
+                  
+                ),
+                ),
+
+//Side Scrolling  Buttons
+                 SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                   child: Padding(
+                     padding: const EdgeInsets.all(8.0),
+                     child: Row(
+                      children: [
+                        GestureDetector (
+                                     onTap: () {},
+                      
+                        child: Container(
+                          padding: const EdgeInsets.all(15),
+                          
+                          
+                          decoration: BoxDecoration(
+                            color: const  Color.fromARGB(255, 8, 223, 195),
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          child: const Text(
+                            "Education",
+                           style: TextStyle(color: Colors.white,fontSize: 20,
+                        fontWeight: FontWeight.w400)
+                          ),
+                          ),
+                      ),
+                      
+                      GestureDetector (
+                                     onTap: () {},
+                      
+                        child: Container(
+                          padding: const EdgeInsets.all(15),
+                          margin: const EdgeInsets.only(left: 20),
+                          
+                          decoration: BoxDecoration(
+                            color: const  Color.fromARGB(255, 8, 223, 195),
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          child: const Text(
+                            "Skills",
+                           style: TextStyle(color: Colors.white,fontSize: 20,
+                        fontWeight: FontWeight.w400)
+                          ),
+                          ),
+                      ),
+                      GestureDetector (
+                                     onTap: () {},
+                      
+                        child: Container(
+                          padding: const EdgeInsets.all(15),
+                          margin: const EdgeInsets.only(left: 20),
+                          
+                          decoration: BoxDecoration(
+                            color: const  Color.fromARGB(255, 8, 223, 195),
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          child: const Text(
+                            "Group Projects",
+                           style: TextStyle(color: Colors.white,fontSize: 20,
+                        fontWeight: FontWeight.w400)
+                          ),
+                          ),
+                      ),
+                      GestureDetector (
+                                     onTap: () {},
+                      
+                        child: Container(
+                          padding: const EdgeInsets.all(15),
+                          margin: const EdgeInsets.only(left: 20),
+                          
+                          decoration: BoxDecoration(
+                            color: const  Color.fromARGB(255, 8, 223, 195),
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          child: const Text(
+                            "Personal Projects",
+                           style: TextStyle(color: Colors.white,fontSize: 20,
+                        fontWeight: FontWeight.w400)
+                          ),
+                          ),
+                      ),
+                                     
+                      ],
+                                     ),
+                   ),
+
+                 ),
+ //Education page     
+                const Text("B.SC HONS INFORMATION TECHNOLOGY SPECIALIZATION - SOFTWARE ENGINEERING MAY 2021 to MAY 2025",
+                textAlign:TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,),
+                ),
+
+                  const SizedBox(
+                    height: 50,
+                  ),
+
+                const Text("AONO NAOKO VOCATIONAL COMPUTER TRAINING INSTITUTE -COMPUTER APPLICATION ASSISTANT COURSE – VERY GOOD PASS  2015",
+                textAlign:TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,),
+                ),
+
+//Skills page
+              const Text("Html\n" 
+              "React js\n" 
+              "Css\n" 
+              "Node js\n"
+              "JavaScript\n" 
+              "Express js\n"
+              "php\n"
+              "Mongo DB\n"
+              "Java/Kotlin\n" 
+              "Post man\n"
+              "python\n" 
+              "C/ C++\n"
+              "My SQL\n"
+              "Wordpress\n"
+              "UI/UX (figma)\n",
+                textAlign:TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,),
+                ),
+            
               ],
             ),
           ),
